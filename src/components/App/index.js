367f47de88af55b88cc9a44f5cb6c975472d0b59
ahmed <troysucks@hotmail.com>
2021-03-29 12:15:37 +0200

    new commit

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../App.css';
+import Landing from '../Landing'
+import Header from '../Header'
+import Footer from '../Footer'
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Landing />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
